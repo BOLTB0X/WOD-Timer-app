07a967738c4738e8a9911a10a1720440199aef1c
B0X <83914919+BOLTB0X@users.noreply.github.com>
2024-09-10 16:31:40 +0900

    - ing
    watch 프레임워크 추가

---
+++ WOD/TimerWatch Watch App/TimerWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerWatchApp.swift
+//  TimerWatch Watch App
+//
+//  Created by KyungHeon Lee on 2024/04/29.
+//
+
+import SwiftUI
+
+@main
+struct TimerWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
